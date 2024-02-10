51203893e0ec4c2ae1345cf2885e1478aaa14ba4
Javier <13680059+Juners@users.noreply.github.com>
2024-02-11 00:16:39 +0100

    Fixed issues in the emotes UI

--- src/routes/emotes/+page.svelte
+++ src/routes/emotes/+page.svelte
@@ -3,283 +3,220 @@
 
 	// import json7tvMock from './json7tvMock.json';
 	// import emoteUsageMock from './emoteUsageMock.json';
-
 	import channelEmotes from './channelEmotes.json';
 
 	import type {
-		EmoteNode,
 		EmoteUsageStats,
 		ChannelEmoteData,
 		Emote7tvData,
-		EmoteUsage
+		EmoteUsage,
+		FlatEmoteUsage
 	} from './types';
 
-	onMount(() => {
-		let lazyLoadTimeout: number | undefined;
-		function lazyLoadWithEvents() {
-			if (lazyLoadTimeout) {
-				clearTimeout(lazyLoadTimeout);
-			}
+	let loading = true;
+	let emoteList: { [id: string]: Emote7tvData | ChannelEmoteData } = {};
+	let emoteUsage: FlatEmoteUsage[] = [];
 
-			lazyLoadTimeout = setTimeout(() => {
-				let emoteListWrapper = document.getElementById('emotes');
-				let lazyLoadImages: NodeListOf<HTMLImageElement> = document.querySelectorAll('img.lazy');
+	async function retrieveEmotes(): Promise<{ [id: string]: Emote7tvData | ChannelEmoteData }> {
+		const call = await fetch('https://7tv.io/v3/emote-sets/62d73f58d8e61c27b053c09a');
+		const seventvEmotes = await call.json();
+		// const seventvEmotes = json7tvMock;
 
-				if (emoteListWrapper) {
-					let scrollTop = emoteListWrapper.scrollTop;
-					lazyLoadImages.forEach((image) => {
-						if (image.offsetTop < window.innerHeight + scrollTop) {
-							image.src = image.dataset.src || '';
-							image.classList.remove('lazy');
-						}
-					});
-
-					if (lazyLoadImages.length == 0) {
-						document.getElementById('emotes')?.removeEventListener('scroll', lazyLoadWithEvents);
-						globalThis.window.removeEventListener('resize', lazyLoadWithEvents);
-						globalThis.window.removeEventListener('orientationChange', lazyLoadWithEvents);
-					}
-				}
-			}, 20);
-		}
-
-		function lazyLoadWithObserver() {
-			if (lazyLoadTimeout) {
-				clearTimeout(lazyLoadTimeout);
-			}
-
-			lazyLoadTimeout = setTimeout(() => {
-				let lazyLoadImages: NodeListOf<HTMLImageElement> = document.querySelectorAll('img.lazy');
-
-				let imageObserver = new IntersectionObserver(function (entries, _observer) {
-					entries.forEach(function (entry) {
-						if (entry.isIntersecting) {
-							let image = entry.target as HTMLImageElement;
-							image.src = image.dataset.src || '';
-							image.classList.remove('lazy');
-							imageObserver.unobserve(image);
-						}
-					});
-				});
-
-				lazyLoadImages.forEach(function (image) {
-					imageObserver.observe(image);
-				});
-			}, 20);
-		}
-
-		// function appendResizeObserver() {
-		//   const resizeObserver = new ResizeObserver((entries) => {
-		//     for (const entry of entries) {
-		//       if (entry) {
-		//         const { width, height } = entry.contentRect;
-
-		//       }
-		//     }
-		//   });
-
-		//   const emotesDiv = document.getElementById("emotes");
-		//   resizeObserver.observe(emotesDiv);
-		// }
-
-		function createEmoteNode(
-			type: '7tv' | 'twitch',
-			emote: EmoteUsageStats,
-			id: string
-		): EmoteNode {
-			let node = null;
-			if (type === '7tv') {
-				node = document.createElement('div');
-				node.classList.add('emote', 'stv');
-
-				const realAmmountNode = document.createElement('div');
-				realAmmountNode.classList.add('smaller');
-				realAmmountNode.appendChild(document.createTextNode(`(${emote.realAmmount})`));
-				node.appendChild(realAmmountNode);
-
-				const img = document.createElement('img');
-				img.dataset.src = `https://cdn.7tv.app/emote/${id}/3x.webp`;
-				img.classList.add('lazy');
-				node.appendChild(img);
-
-				const ammount = document.createElement('div');
-				ammount.appendChild(document.createTextNode(`${emote.ammount}`));
-				node.appendChild(ammount);
-			} else if (type === 'twitch') {
-				node = document.createElement('div');
-				node.classList.add('emote', 'twitch');
-
-				const realAmmountNode = document.createElement('div');
-				realAmmountNode.classList.add('smaller');
-				realAmmountNode.appendChild(document.createTextNode(`(${emote.realAmmount})`));
-				node.appendChild(realAmmountNode);
-
-				const img = document.createElement('img');
-				img.dataset.src = `https://static-cdn.jtvnw.net/emoticons/v2/${id}/default/dark/3.0`;
-				img.classList.add('lazy');
-				node.appendChild(img);
-
-				const ammount = document.createElement('div');
-				ammount.appendChild(document.createTextNode(`${emote.ammount}`));
-				node.appendChild(ammount);
-			}
-
-			return { type, data: emote, node };
-		}
-
-		let allEmotes: { chn: ChannelEmoteData[]; stv: Emote7tvData[] };
-		async function retrieveEmotes() {
-			let seventvEmotes: Emote7tvData[] = [];
-			try {
-				const call = await fetch('https://7tv.io/v3/emote-sets/62d73f58d8e61c27b053c09a');
-				const json = await call.json();
-				// const json = json7tvMock;
-
-				seventvEmotes = json.emotes.map((emote: any) => {
-					return {
-						realAmmount: 0,
-						ammount: 0,
-						data: {
-							id: emote.data.id,
-							name: emote.name,
-							animated: emote.data.animated,
-							listed: emote.data.listed
-						}
-					};
-				});
-			} catch (e) {
-				return Promise.reject();
-			}
-
-			allEmotes = { chn: channelEmotes, stv: seventvEmotes };
-
-			return Promise.resolve();
-		}
-
-		async function initialModcheck() {
-			// let emoteUsage: EmoteUsage = emoteUsageMock;
-			const response = await fetch(
-				'https://zle5lrm7f7fnrc2di5fw7wnrfe0uwunk.lambda-url.eu-west-2.on.aws'
-			);
-			let emoteUsage: EmoteUsage = (await response.json()).emoteUsage;
-
-			if (emoteUsage) {
-				const loadingNode = document.body.querySelector('main #loading');
-
-				const emotesHolder = document.body.querySelector('main #emotes');
-				if (emotesHolder) {
-					if (loadingNode) {
-						loadingNode.classList.add('hidden');
-					}
-
-					emotesHolder.classList.remove('hidden');
-
-					const emotes: EmoteNode[] = [];
-					Object.entries(emoteUsage.twitch).forEach(function ([id, twiEmote]) {
-						emotes.push(createEmoteNode('twitch', twiEmote, id));
-					});
-
-					Object.entries(emoteUsage.seventv).forEach(function ([id, stvEmote]) {
-						emotes.push(createEmoteNode('7tv', stvEmote, id));
-					});
-
-					emotes
-						.sort((a, b) => {
-							let sort = b.data.ammount - a.data.ammount;
-							if (sort === 0) sort = b.data.realAmmount - a.data.realAmmount;
-							if (sort === 0) sort = b.type.length - a.type.length; // Twitch is longer, so goes first
-							return sort;
-						})
-						.forEach(({ node }) => {
-							if (node) emotesHolder.appendChild(node);
-						});
-
-					allEmotes.chn.forEach((emote) => {
-						if (!emoteUsage.twitch[emote.data.id]) {
-							const emoteNode = createEmoteNode('twitch', emote, emote.data.id).node;
-							if (emoteNode) {
-								emotesHolder.appendChild(emoteNode);
-							}
-						}
-					});
-
-					allEmotes.stv.forEach((emote) => {
-						if (!emoteUsage.seventv[emote.data.id]) {
-							const emoteNode = createEmoteNode('7tv', emote, emote.data.id).node;
-							if (emoteNode) {
-								emotesHolder.appendChild(emoteNode);
-							}
-						}
-					});
-				}
-			}
-		}
-
-		retrieveEmotes().then(() => {
-			initialModcheck();
+		const emoteList: { [id: string]: Emote7tvData | ChannelEmoteData } = {};
+		channelEmotes.data.forEach((emote) => {
+			emoteList[`twitch_${emote.id}`] = emote;
+		});
+		seventvEmotes.emotes.forEach((emote: Emote7tvData) => {
+			emoteList[`7tv_${emote.id}`] = emote;
 		});
 
-		if ('IntersectionObserver' in window) {
-			lazyLoadWithObserver();
-		} else {
-			lazyLoadWithEvents();
+		return emoteList;
+	}
 
-			document.getElementById('emotes')?.addEventListener('scroll', lazyLoadWithEvents);
-			globalThis.window.addEventListener('resize', lazyLoadWithEvents);
-			globalThis.window.addEventListener('orientationChange', lazyLoadWithEvents);
+	async function fetchEmoteUsage(): Promise<FlatEmoteUsage[]> {
+		const response = await fetch(
+			'https://zle5lrm7f7fnrc2di5fw7wnrfe0uwunk.lambda-url.eu-west-2.on.aws'
+		);
+		const emoteUsage: EmoteUsage = (await response.json()).emoteUsage;
+		// const emoteUsage: EmoteUsage = emoteUsageMock;
+
+		const emoteList: { id: string; type: 'twitch' | '7tv'; stats: EmoteUsageStats }[] = [];
+		Object.entries(emoteUsage.twitch).forEach(([id, stats]) => {
+			emoteList.push({ id, type: 'twitch', stats });
+		});
+		Object.entries(emoteUsage.seventv).forEach(([id, stats]) => {
+			emoteList.push({ id, type: '7tv', stats });
+		});
+
+		const sortedUsage = emoteList.sort((a, b) => {
+			let sort = b.stats.ammount - a.stats.ammount;
+			if (sort === 0) sort = b.stats.realAmmount - a.stats.realAmmount;
+			if (sort === 0) sort = b.type.length - a.type.length; // Twitch is longer, so goes first
+			return sort;
+		});
+
+		return sortedUsage;
+	}
+
+	function getImageSource(emote: FlatEmoteUsage) {
+		let url = '';
+		if (emote.type === 'twitch') {
+			url = `https://static-cdn.jtvnw.net/emoticons/v2/${emote.id}/default/dark/3.0`;
+		} else if (emote.type === '7tv') {
+			url = `https://cdn.7tv.app/emote/${emote.id}/3x.webp`;
 		}
+
+		return url;
+	}
+
+	let lazyLoadTimeout: number | undefined;
+	function lazyLoadWithEvents() {
+		if (lazyLoadTimeout) {
+			clearTimeout(lazyLoadTimeout);
+		}
+
+		lazyLoadTimeout = setTimeout(() => {
+			let emoteListWrapper = document.getElementById('emote-list');
+			let lazyLoadImages: NodeListOf<HTMLImageElement> = document.querySelectorAll('img.lazy');
+
+			if (emoteListWrapper) {
+				let scrollTop = emoteListWrapper.scrollTop;
+				lazyLoadImages.forEach((image) => {
+					if (image.offsetTop < window.innerHeight + scrollTop) {
+						image.src = image.dataset.src || '';
+						image.classList.remove('lazy');
+					}
+				});
+
+				if (lazyLoadImages.length == 0) {
+					document.getElementById('emote-list')?.removeEventListener('scroll', lazyLoadWithEvents);
+					window.removeEventListener('resize', lazyLoadWithEvents);
+					window.removeEventListener('orientationChange', lazyLoadWithEvents);
+				}
+			}
+		}, 20);
+	}
+
+	function lazyLoadWithObserver() {
+		if (lazyLoadTimeout) {
+			clearTimeout(lazyLoadTimeout);
+		}
+
+		lazyLoadTimeout = setTimeout(() => {
+			let lazyLoadImages: NodeListOf<HTMLImageElement> = document.querySelectorAll('img.lazy');
+
+			let imageObserver = new IntersectionObserver(function (entries, _observer) {
+				entries.forEach(function (entry) {
+					if (entry.isIntersecting) {
+						let image = entry.target as HTMLImageElement;
+						image.src = image.dataset.src || '';
+						image.classList.remove('lazy');
+						imageObserver.unobserve(image);
+					}
+				});
+			});
+
+			lazyLoadImages.forEach(function (image) {
+				imageObserver.observe(image);
+			});
+		}, 20);
+	}
+
+	onMount(() => {
+		async function fetchData() {
+			emoteList = await retrieveEmotes();
+			emoteUsage = await fetchEmoteUsage();
+
+			loading = false;
+		}
+
+		fetchData().then(() => {
+			if (window.IntersectionObserver) {
+				lazyLoadWithObserver();
+			} else {
+				lazyLoadWithEvents();
+
+				document.getElementById('emote-list')?.addEventListener('scroll', lazyLoadWithEvents);
+				window.addEventListener('resize', lazyLoadWithEvents);
+				window.addEventListener('orientationChange', lazyLoadWithEvents);
+			}
+		});
 	});
 </script>
 
-<div id="loading">
-	<span class="uppercase">C</span>hecking...
-	<img
-		class="smaller"
-		src="https://cdn.7tv.app/emote/60eefb20119bd109472f7f4b/4x.webp"
-		title="modCheck"
-		alt="modCheck"
-	/>
-</div>
+{#if loading}
+	<div id="loading">
+		<span class="uppercase">C</span>hecking...
+		<img
+			class="smaller"
+			src="https://cdn.7tv.app/emote/60eefb20119bd109472f7f4b/4x.webp"
+			title="modCheck"
+			alt="modCheck"
+		/>
+	</div>
+{:else}
+	<div id="emote-list">
+		{#each emoteUsage as emote}
+			<div class={`emote ${emote.type === '7tv' ? 'stv' : emote.type}`}>
+				<div class="real-ammount" title="The total ammount sent">({emote.stats.realAmmount})</div>
 
-<div id="emotes" class="hidden"></div>
+				<img class="lazy" data-src={getImageSource(emote)} alt="emote" />
+				<div>{emote.stats.ammount}</div>
+			</div>
+		{/each}
+	</div>
+{/if}
 
 <style>
-	.hidden {
-		display: none !important;
-	}
-
 	.smaller {
 		font-size: 0.9rem;
 	}
 
-	#emotes {
+	#emote-list {
 		display: flex;
 		flex-wrap: wrap;
 		overflow: scroll;
 		margin: 0 15px;
+		justify-content: center;
+		gap: 10px;
+		box-sizing: border-box;
 	}
 
-	:global(#emotes .emote.stv) {
+	#emote-list .emote.stv {
 		color: #29b6f6;
 	}
-	:global(#emotes .emote.twitch) {
+
+	#emote-list .emote.stv:hover {
+		background-color: rgb(41, 182, 246, 0.3);
+	}
+	#emote-list .emote.twitch {
 		color: #8a47eb;
 	}
+	#emote-list .emote.twitch:hover {
+		background-color: rgb(138, 71, 235, 0.3);
+	}
 
-	:global(#emotes img) {
+	#emote-list img {
 		max-height: 100px;
 		max-width: 100%;
 	}
 
-	:global(#emotes > div) {
+	.emote {
 		display: flex;
 		flex-direction: column;
 		justify-content: space-between;
 		align-items: center;
-		width: 155px;
-		height: 155px;
-		padding: 5px 0;
+		float: left;
+		width: 160px;
+		height: 160px;
+		padding: 5px;
+		box-sizing: border-box;
+		cursor: default;
 
 		border: 1px solid #db1e83;
+	}
+
+	.emote .real-ammount {
+		font-size: 0.6em;
 	}
 </style>
--- src/routes/emotes/types.ts
+++ src/routes/emotes/types.ts
@@ -8,32 +8,60 @@ export type EmoteUsage = {
 	seventv: { [emoteId: string]: EmoteUsageStats };
 };
 
+export type FlatEmoteUsage = { id: string; type: 'twitch' | '7tv'; stats: EmoteUsageStats };
+
 export type Emote7tvData = {
-	realAmmount: number;
-	ammount: number;
+	id: string;
+	name: string;
+	flags: number;
+	timestamp: number;
+	actor_id: string;
 	data: {
 		id: string;
 		name: string;
-		animated: boolean;
+		flags: number;
+		tags?: string[];
+		lifecycle: number;
+		state: string[];
 		listed: boolean;
+		animated: boolean;
+		owner: {
+			id: string;
+			username: string;
+			display_name: string;
+			avatar_url?: string;
+			style: {
+				color?: number;
+			};
+			roles?: string[];
+		};
+		host: {
+			url: string;
+			files: {
+				name: string;
+				static_name: string;
+				width: number;
+				height: number;
+				frame_count: number;
+				size: number;
+				format: string;
+			}[];
+		};
 	};
 };
 
 export type ChannelEmoteData = {
-	realAmmount: number;
-	ammount: number;
-	data: {
-		id: string;
-		tier: number;
-		name: string;
-		scale: string[];
-		theme_mode: string[];
-		format: string[];
+	id: string;
+	name: string;
+	images: {
+		url_1x: string;
+		url_2x: string;
+		url_4x: string;
 	};
-};
-
-export type EmoteNode = {
-	type: '7tv' | 'twitch';
-	data: { ammount: number; realAmmount: number };
-	node: HTMLDivElement | null;
+	tier: string;
+	emote_type: string;
+	emote_set_id: string;
+	format: string[];
+	scale: string[];
+	theme_mode: string[];
 };
